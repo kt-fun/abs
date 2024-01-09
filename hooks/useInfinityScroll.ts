2c950428b02d13e1fae2898c9023c4050d946d32
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2024-01-09 19:14:11 +0800

    fix mobile scroll issue

--- hooks/useInfinityScroll.ts
+++ hooks/useInfinityScroll.ts
@@ -1,21 +1,24 @@
 import { MutableRefObject, useCallback, useEffect, useState } from "react";
 
 export const useInfinityScroll = () => {
-    const [reachedBottom,setReachedBottom] = useState(false);
+  // for mobile devices, may have some compatibility issues,not exactly equal,
+  // so if distance to bottom is less than 10px then we consider it as reached bottom
+    const [almostReachedBottom,setAlmostReachedBottom] = useState(false);
     const [top,setTop] = useState(0);
     const handleScroll = useCallback(() => {
         setTop(document.documentElement.scrollTop);
         if(document.body.clientHeight <= window.innerHeight) {
-            reachedBottom || setReachedBottom(true);
+          almostReachedBottom || setAlmostReachedBottom(true);
         }else if (
-          window.innerHeight + document.documentElement.scrollTop ===
+          window.innerHeight + document.documentElement.scrollTop + 10 >=
           document.documentElement.offsetHeight
         ) {
-            reachedBottom || setReachedBottom(true);
+          almostReachedBottom || setAlmostReachedBottom(true);
         }else {
-            reachedBottom && setReachedBottom(false);
+          almostReachedBottom && setAlmostReachedBottom(false);
         }
-      }, [reachedBottom]);
+      }, [almostReachedBottom]);
+
     useEffect(() => {
         const interval = setInterval(() => {
           handleScroll();
@@ -28,8 +31,8 @@ export const useInfinityScroll = () => {
       }, [handleScroll]);
 
         return {
-            reachedBottom,
-            setReachedBottom,
+            reachedBottom:almostReachedBottom,
+            setReachedBottom:setAlmostReachedBottom,
             showScrollToTop: top > 100,
             scrollToTop: () => {
               window.scrollTo({ top: 0, behavior: "smooth" });
