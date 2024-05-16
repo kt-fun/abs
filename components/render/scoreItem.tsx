e3f1f1e7cdd5220c7cc3206be00665a466b9db57
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2024-05-16 08:50:28 +0800

    :lipstick: improve ui for scoresaber & beatleader panel

--- components/render/scoreItem.tsx
+++ components/render/scoreItem.tsx
@@ -15,7 +15,7 @@ const diffConv = (diff:string) => {
   return "E"
 }
 const getModifiers = (modifiers:string)=> {
-  return modifiers?modifiers.split(','):['None']
+  return modifiers?modifiers.split(','):['']
 }
 export default function ScoreItem(
 {
@@ -26,10 +26,10 @@ export default function ScoreItem(
 ) {
   return (
     <div className={'relative'}>
-      <div className={"rounded-lg flex bg-black/[.4] space-x-2  backdrop-blur-md"}>
-        <Avatar src={scoreItem.leaderboard.coverImage} className={"rounded-md h-20 w-20"}/>
+      <div className={"rounded-lg flex bg-black/[.4] space-x-2  backdrop-blur-md overflow-hidden"}>
+        <img loading={'eager'} src={scoreItem.leaderboard.coverImage} className={"rounded-md h-20 w-20"}/>
         <div className={"flex flex-col justify-between py-0.5"}>
-          <div className={"font-semibold text-xs text-ellipsis overflow-hidden line-clamp-1"}>
+          <div className={"font-semibold text-xs text-ellipsis overflow-hidden line-clamp-1 break-all"}>
             {scoreItem.leaderboard.songName}
           </div>
           <div className={"text-xs flex items-center space-x-2 *:flex *:items-center *:space-x-1 "}>
@@ -50,20 +50,25 @@ export default function ScoreItem(
             <span>{(scoreItem.score.baseScore / scoreItem.leaderboard.maxScore * 100).toFixed(2)}%</span>
             <span className={"text-orange-200"}>{scoreItem.score.pp.toFixed(1)}PP</span>
           </div>
-          <div className="text-xs flex flex-wrap space-x-2 items-center">
-            <Wrench className="h-3 w-3"/>
-            {
-              getModifiers(scoreItem.score.modifiers).map(modifier => (
-                <span key={modifier}>{modifier}</span>
-              ))
-            }
-          </div>
+          {
+            getModifiers(scoreItem.score.modifiers).length > 0 &&
+            (
+              <div className="text-xs flex flex-wrap space-x-2 items-center">
+                <Wrench className="h-3 w-3"/>
+                {
+                  getModifiers(scoreItem.score.modifiers).map(modifier => (
+                    <span key={modifier}>{modifier}</span>
+                  ))
+                }
+              </div>
+            )
+          }
         </div>
       </div>
-      <div className="absolute right-1 bottom-1 text-white flex text-xs space-x-2">
+      <div className="absolute right-1 bottom-1 text-white flex text-xs space-x-1 items-center">
         {
           scoreItem.score.fullCombo &&
-            <span className="from-red-300 to-red-300 bg-gradient-to-r bg-clip-text text-transparent">FC</span>
+            <span className="from-blue-300 to-red-300 bg-gradient-to-r bg-clip-text text-transparent">FC</span>
         }
       </div>
     </div>
