3647b28d00d142b71189ed65275eca1d0877a180
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2024-11-21 17:01:09 +0800

    refactor

--- app/tmp/lb/layout.tsx
+++ app/tmp/lb/layout.tsx
@@ -1,20 +1,14 @@
-import React, {Suspense} from "react";
-import dynamic from "next/dynamic";
-const DynamicOps = dynamic(()=> import('@/app/tmp/lb/ops'), {
-  loading: () => <p>Loading...</p>,
-})
+import React from "react";
+// import dynamic from "next/dynamic";
+// const DynamicOps = dynamic(()=> import('@/app/tmp/lb/ops'), {
+//   loading: () => <p>Loading...</p>,
+// })
 export default function Layout(
   { children }: { children?: React.ReactNode },
 ) {
-
-
-  const data = {
-    background: "https://loliapi.com/acg/pe",
-    size: 20,
-  }
   return (
     <div className="flex h-full flex-col  md:overflow-hidden">
-        <DynamicOps/>
+        {/*<DynamicOps/>*/}
       {/*<OpsPanelContext.Provider value={data}>*/}
         <div className="flex-grow md:overflow-y-auto my-2">{children}</div>
       {/*</OpsPanelContext.Provider>*/}
